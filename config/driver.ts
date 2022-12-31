63fe026343f1df4ddee56586ee339bab2f0f8bda
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-01 02:21:00 +0900

    wip

--- config/driver.ts
+++ config/driver.ts
@@ -8,5 +8,30 @@ export type PostgresConnectionOptions = {
     password: string // db password
   }
 }
-const n = new net.Socket()
-n.connect(5439, 'xxx')
+
+const call = async (host: number, port: string): Promise<void> => {
+  const client = new net.Socket()
+  await client.connect(host, port, function () {
+    console.log('接続: ' + host + ':' + port)
+
+    // サーバーへメッセージを送信
+    client.write('ハロー')
+  })
+
+  // サーバーからメッセージを受信したら、その内容を表示する
+  client.on('data', function (data) {
+    console.log(data)
+  })
+
+  // 接続が切断されたら、その旨をメッセージで表示する
+  client.on('close', function () {
+    console.log('切断')
+  })
+}
+
+call(5439, '127.0.0.1')
+
+/**  FYI
+ * Node.js で TCP/IP クライアントを作る
+ * http://dotnsf.blog.jp/archives/1070761196.html
+ */
