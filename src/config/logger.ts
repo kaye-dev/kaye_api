2eb8f406523d126b5aaee0353bc4334f87bb1ecc
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-01 23:50:00 +0900

    logger

---
+++ src/config/logger.ts
@@ -0,0 +1,4 @@
+import { getLogger } from 'log4js';
+export const systemLogger = getLogger('system');
+export const accessLogger = getLogger('access');
+export const errorLogger = getLogger('error');
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import { app } from './config/router';
 import { configure } from 'log4js';
+import { address } from 'ip';
+import { accessLogger } from './config/logger';
 configure('./log4js.config.json');
+accessLogger.info(address());
 
 const PORT = 3000;
 app.use(express.json());
