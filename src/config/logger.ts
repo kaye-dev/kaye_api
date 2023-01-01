e35be5ec32723f154e85761131c9cb846f772bfa
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-02 00:41:08 +0900

    logger

--- src/config/logger.ts
+++ src/config/logger.ts
@@ -6,3 +6,7 @@ export const errorLogger = getLogger('error');
 export const getReferer = (referer?: string): string => {
   return referer ? `referer: ${referer}` : 'referer: -';
 };
+
+export const controllerLogFormat = (controllerAction: string, ip: string, referer: string): string => {
+  return `controller\n[Controller]: ${controllerAction}\n[IP] ${ip}\n[Referer] ${referer}`;
+};
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express';
 import { address } from 'ip';
-import { accessLogger, getReferer } from '../config/logger';
+import { accessLogger, getReferer, controllerLogFormat } from '../config/logger';
 
 export const usersController = (_req: Request, res: Response) => {
-  accessLogger.info(address(), getReferer(_req.headers.referer), 'Processing by: usersController#index');
+  accessLogger.info(controllerLogFormat('usersController#index', address(), getReferer(_req.headers.referer)));
 
   return res.status(200).send({
     message: 'usersController',
