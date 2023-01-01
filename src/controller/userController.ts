4ace6a7ea3b83423d2559800c2b62a26a4b7046b
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-02 01:00:34 +0900

    logger

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from 'express';
 import { address } from 'ip';
-import { accessLogger, getReferer, controllerLogFormat } from '../config/logger';
+import { accessLogger, getReferer, controllerLogFormat, countLines } from '../config/logger';
+import path from 'path';
+const targetFilePath = path.resolve(__dirname, './userController.ts');
 
 export const usersController = (_req: Request, res: Response) => {
-  accessLogger.info(controllerLogFormat('usersController#index', address(), getReferer(_req.headers.referer)));
+  console.log('targetFilePath', targetFilePath);
+  countLines(targetFilePath, function (error: Error, numberOfLines: number) {
+    console.log('counter:', numberOfLines);
+  }),
+    accessLogger.info(controllerLogFormat('usersController#index', address(), getReferer(_req.headers.referer)));
 
   return res.status(200).send({
     message: 'usersController',
