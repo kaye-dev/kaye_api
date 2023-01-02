140d27002bd4f78d37ad2b58a30c37786cd306ff
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-02 20:24:17 +0900

    wip

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -3,7 +3,6 @@ import { getLogger } from 'log4js';
 import { loggerKind } from '../config/logger';
 
 export const usersController = (_req: Request, res: Response) => {
-  getLogger(loggerKind.access).info('success');
   const err = new Error('something error');
   getLogger(loggerKind.error).error(err.name, err.message);
 
