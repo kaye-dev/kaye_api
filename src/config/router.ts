8935e453141d48a50b6a0279cd59494c704c22b7
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-07 06:50:55 +0900

    wip

--- src/config/router.ts
+++ src/config/router.ts
@@ -2,8 +2,23 @@ import express, { Application, Request, Response } from 'express';
 import { usersController } from '../controller/userController';
 import { adminsController } from '../controller/adminController';
 import { rootsController } from '../controller/rootsController';
+import { address } from 'ip';
+import { getLogger } from 'log4js';
+import { loggerKind } from '../config/logger';
 
+// FYI: https://www.wakuwakubank.com/posts/632-nodejs-express-overview/#index_id2
 export const app: Application = express();
-app.use('/users', usersController);
-app.use('/admins', adminsController);
-app.use('/', rootsController);
+
+// 全リクエスト対象::logging
+app.use((req: Request, _: Response, next) => {
+  getLogger(loggerKind.access).info(address(), req.headers.referer, 'logging');
+  next();
+});
+
+app.get('/users', usersController);
+app.get('/admins', adminsController);
+app.get('/roots', rootsController);
+
+app.get('/', function (_req: Request, res: Response) {
+  res.send('Hello from API::V1 root route');
+});
