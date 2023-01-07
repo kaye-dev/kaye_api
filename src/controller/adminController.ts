7542d281685563c36bed3307d6a84c418967515d
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-07 17:37:17 +0900

    wip

--- src/controller/adminController.ts
+++ src/controller/adminController.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express';
-import { address } from 'ip';
-import { getLogger } from 'log4js';
-import { loggerKind } from '../config/logger';
 
 export const adminsController = (_req: Request, res: Response) => {
-  getLogger(loggerKind.access).info(address(), _req.headers.referer, 'adminsController');
-
   return res.status(200).send({
     message: 'adminsController',
   });
--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,11 +1,6 @@
 import { Request, Response } from 'express';
-import { getLogger } from 'log4js';
-import { loggerKind } from '../config/logger';
 
 export const usersController = (_req: Request, res: Response) => {
-  const err = new Error('something error');
-  getLogger(loggerKind.error).error(err.name, err.message);
-
   return res.status(200).send({
     message: 'usersController',
   });
