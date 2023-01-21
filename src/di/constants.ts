98f30be608052866faa04ef9889116fa54617641
keien-nishikawa <keien.nishikawa@clubx.jp>
2023-01-22 04:43:47 +0900

    wip

--- src/di/constants.ts
+++ src/di/constants.ts
@@ -1,3 +1,19 @@
-export const di = {
+// controllers
+export const dic = {
+  UserController: 'UserController',
+};
+
+// models
+export const dim = {
+  UserModel: 'UserModel',
+};
+
+// decorators
+export const did = {
   UserDecorator: 'UserDecorator',
+};
+
+// serializers
+export const dis = {
+  UserSerializer: 'UserSerializer',
 };
--- src/di/di.ts
+++ src/di/di.ts
@@ -1,9 +1,25 @@
 import { container } from 'tsyringe';
-import { di } from './constants';
+import { dic, did, dim, dis } from './constants';
 import { UserDecoratorImpl } from '../decorators/userDecorator';
 
 export const registerDependencies = () => {
-  container.register(di.UserDecorator, {
+  // controllers
+  container.register(dic.UserController, {
+    useClass: UserDecoratorImpl,
+  });
+
+  // models
+  container.register(dim.UserModel, {
+    useClass: UserDecoratorImpl,
+  });
+
+  // decorators
+  container.register(did.UserDecorator, {
+    useClass: UserDecoratorImpl,
+  });
+
+  // serializers
+  container.register(dis.UserSerializer, {
     useClass: UserDecoratorImpl,
   });
 };
